fe980efa2770ad49307681ba31f369b6d0d29423
夜光 <yizhimoshushi@outlook.com>
2026-03-21 07:19:44 +0800

    first commit

---
+++ install.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ========================
+echo      python bag install
+echo ========================
+
+pip install pygame numpy PyOpenGL PyOpenGL_accelerate pywavefront requests bs4 psutil Babel                        -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+pause
